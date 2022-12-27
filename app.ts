fffec5f6698eb9db3f40d71826e774ef4b14493a
Iryna Moiseieva <i.moiseieva@godeltech.com>
2022-12-27 14:22:25 +0100

    first commit

--- app.ts
+++ app.ts
@@ -1,6 +1,7 @@
 const button = document.querySelector("button");
 const input1 = document.getElementById("num1")! as HTMLInputElement;
 const input2 = document.getElementById("num2")! as HTMLInputElement;
+const input3 = document.getElementById("num3")! as HTMLInputElement;
 
 function add(num1: number, num2: number) {
   return num1 + num2;
